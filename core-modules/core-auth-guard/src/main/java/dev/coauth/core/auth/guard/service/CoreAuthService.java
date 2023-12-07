ce25db4867213f640b5df86384e335d4c0852f52
Godwin Pinto <godwin.pinto86@gmail.com>
2023-12-07 12:17:14 +0530

    refactor: quarkus microservices combined

--- core-modules/core-auth-guard/src/main/java/dev/coauth/core/auth/guard/service/CoreAuthService.java
+++ core-modules/core-auth-guard/src/main/java/dev/coauth/core/auth/guard/service/CoreAuthService.java
@@ -2,6 +2,7 @@ package dev.coauth.core.auth.guard.service;
 
 import dev.coauth.core.auth.guard.cache.local.ApiKeyCacheBean;
 import dev.coauth.core.auth.guard.entity.CoreApplicationMstrEntity;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 import io.quarkus.security.UnauthorizedException;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -13,6 +14,7 @@ public class CoreAuthService {
     @Inject
     ApiKeyCacheBean apiKeyCacheBean;
 
+    @WithSpan("CoreAuthService.authenticateApi")
     public Uni<CoreApplicationMstrEntity> authenticateApi(String apiKey) {
         return Uni.createFrom().item(apiKeyCacheBean.getValue(apiKey))
                 .onItem().transformToUni(coreApplicationMstrEntity -> {
